e7db91cf51ceecf1d6715460393a14445485a800
lahmxu <lahmxj@gmail.com>
2022-08-17 00:46:49 +0800

    feat: 增加 byte-buddy 拦截方法读取变量值

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 ## javaAgent demo
 ### 模块介绍
-- agent 模块负责实现agent功能, 目前是简单的打印入参
+- agent 模块负责实现agent功能
 - demo 模块是一个简单的 springboot 项目, 用于测试
   
 ### 执行步骤
 1. 切换到 javaAgent-demo 目录下执行`mvn clean package -DskipTests`, 等待编译完成
 2. 执行`java -javaagent:./agent/target/agent-0.0.1-SNAPSHOT.jar="hello lahmxu" -jar demo/target/demo-0.0.1-SNAPSHOT.jar`
 3. 控制台输出如下表示成功, agent中修改的方法成功执行
-  ```log
-    -------------------agent start-------------------
-    ------ premain 方法两个入参 ------ agentArgs:hello lahmxu inst:sun.instrument.InstrumentationImpl@33909752
-    ------ 此处可以进行字节码操作 ------ 
-    -------------------agent end-------------------
-    ------ main ------
+  ```yaml
+  # 输出：
+  enter method call
+  version:1.0 // 执行方法的输出
+  [CallMethodDelegation] version:1.0 // 拦截方法的输出
+  exit method get
   ```
+
--- agent/src/main/java/com/lahmxu/agent/AgentDemo.java
+++ agent/src/main/java/com/lahmxu/agent/AgentDemo.java
@@ -1,5 +1,9 @@
 package com.lahmxu.agent;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+
 import java.lang.instrument.Instrumentation;
 
 public class AgentDemo {
@@ -8,19 +12,21 @@ public class AgentDemo {
      * 该方法在 main 方法之前运行, 与 main 方法运行在同一个 JVM 中
      */
     public static void premain(String agentArgs, Instrumentation inst) {
-        /**
-         * 此处可以通过 javassist 修改字节码操作, 完成修改编码的功能
-         */
-        System.out.println("-------------------agent start-------------------");
-        System.out.println("------ premain 方法两个入参 ------ agentArgs:" + agentArgs + " inst:" + inst.toString());
-        System.out.println("------ 此处可以进行字节码操作 ------ ");
-        System.out.println("-------------------agent end-------------------");
+        init(agentArgs, inst);
     }
 
-    /**
-     * 如果不存在 {@link AgentDemo#premain(String, Instrumentation)}, 则会执行本方法
-     */
-    public static void premain(String agentArgs) {
-        System.out.println("------ premain 方法一个参数 ------ agentArgs:" + agentArgs);
+    public static void init(String args, Instrumentation inst) {
+        System.out.println("-------------------agent start-------------------");
+
+        // 拦截test方法执行，拦截之后执行 CallMethodDelegation 中的方法
+        new AgentBuilder.Default()
+                .type(ElementMatchers.any())
+                .transform((builder, typeDescription, classLoader, module) -> builder
+                        .method(ElementMatchers.named("test"))
+                        .intercept(MethodDelegation.to(CallMethodDelegation.class))
+                )
+                .installOn(inst);
+
+        System.out.println("-------------------agent end-------------------");
     }
 }
