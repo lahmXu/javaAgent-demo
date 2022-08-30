eb0a755a1a3e114b0d25e2735a13158fb16ebf95
lahmxu <lahmxj@gmail.com>
2022-08-30 13:51:56 +0800

    fix: test

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 mvn clean install -DskipTests -Dcheckstyle.skip=true -Drat.skip=true
 
 java -javaagent:./agent/target/agent-0.0.1-SNAPSHOT.jar\
-="./agent/target/agent-0.0.1-SNAPSHOT.jar"\
- -jar demo/target/demo-0.0.1-SNAPSHOT.jar
+="./agent/target/agent-0.0.1-SNAPSHOT.jar" \
+-jar demo/target/demo-0.0.1-SNAPSHOT.jar
