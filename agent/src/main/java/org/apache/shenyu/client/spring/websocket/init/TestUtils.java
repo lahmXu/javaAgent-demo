667eeb916780799f9a6f43796bf8a2854e8d35bc
lahmxu <lahmxj@gmail.com>
2022-08-31 12:07:34 +0800

    fix: update

--- agent/src/main/java/org/apache/shenyu/client/spring/websocket/init/TestUtils.java
+++ agent/src/main/java/org/apache/shenyu/client/spring/websocket/init/TestUtils.java
@@ -2,7 +2,12 @@ package org.apache.shenyu.client.spring.websocket.init;
 
 public class TestUtils {
 
+    // 正确示例：只能新增静态私有方法
+    private static String test2() {
+        return "test2";
+    }
+
     public static void test() {
-        System.out.println("Agent TestUtils test()");
+        System.out.println("Agent TestUtils test(), name:" + test2());
     }
 }
